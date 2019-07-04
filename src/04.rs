7d7ad2bd4d7894a48adfc5230f72fb20e7bebf15
bi3mer <colan@comcast.net>
2019-07-04 10:08:41 -0700

    day 04

--- src/04.rs
+++ src/04.rs
@@ -1,9 +1,28 @@
-extern crate crypto;
-use crypto::md5::Md5;
+extern crate md5;
+
+use indicatif::ProgressBar;
+
+// fn find_value(secret_key: &str) -> String {
+    
+// }
 
 fn main() {
     let input = "ckczppom";
+    let bar = ProgressBar::new_spinner();
 
-    let digest = md5::compute(b"abcdefghijklmnopqrstuvwxyz");
-    assert_eq!(format!("{:x}", digest), "c3fcd3d76192e4007dfb496cca67e13b");
+    let mut i = 0;
+    loop {
+        let input_str = format!("{}{}", input, i.to_string());
+
+        let digest = md5::compute(&input_str);
+        let stringified_md5 = format!("{:x}", digest);
+
+        if stringified_md5.starts_with("000000") {
+            bar.finish_with_message(&input_str);
+            break
+        }
+
+        i += 1;
+        bar.tick();
+    }
 }
