1d5f8c2045b7ef8cfcfc08c39424def6b0e11d49
bi3mer <colan@comcast.net>
2020-06-06 14:29:38 -0400

    day 10 parts 1 and 2

---
+++ src/10.rs
@@ -0,0 +1,38 @@
+fn part1(input: String) -> String {
+    let mut result = String::from("");
+    let mut count = 1;
+    
+    let mut char_iterator = input.chars();
+    let mut previous_char = char_iterator.next().unwrap();
+
+    for character in char_iterator {
+        if character != previous_char {
+            result.push_str(&count.to_string());
+            result.push_str(&previous_char.to_string());
+
+            previous_char = character;
+            count = 1;
+        } else {
+            count += 1;
+        }
+    }
+
+    result.push_str(&count.to_string());
+    result.push_str(&previous_char.to_string());
+
+    result
+}
+
+fn main() {
+    println!("{}", part1(String::from("1")) == String::from("11"));
+    println!("{}", part1(String::from("11")) == String::from("21"));
+    println!("{}", part1(String::from("21")) == String::from("1211"));
+    println!("{}", part1(String::from("1211")) == String::from("111221"));
+
+    let mut input = String::from("1113122113");
+    for i in 0..50 {
+        input = part1(input);
+    }
+
+    println!("{}", input.len());
+}
